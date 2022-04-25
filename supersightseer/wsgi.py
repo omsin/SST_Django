fd7a4fc60857ad79b862f7033309f578b416f33c
omsin <omsin4c@gmail.com>
2022-04-26 02:45:12 +0700

    Test test

---
+++ supersightseer/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for supersightseer project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'supersightseer.settings')
+
+application = get_wsgi_application()
